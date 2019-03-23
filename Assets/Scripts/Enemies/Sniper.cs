46444098ae22d824d9d6dcbbd2f1ae7dc79991b9
Patrick <pwcarney@bu.edu>
2019-03-23 11:13:46 -0400

    Added sniper sprites, tweaked how sniping works

--- Assets/Scripts/Enemies/Sniper.cs
+++ Assets/Scripts/Enemies/Sniper.cs
@@ -4,6 +4,7 @@ using UnityEngine;
 
 public class Sniper : MonoBehaviour
 {
+    bool firing = false;
     float charge_time = 0f;
     float last_shot;
 
@@ -13,14 +14,22 @@ public class Sniper : MonoBehaviour
 
     Transform child;
     SpriteRenderer laser_renderer;
+    public Color charging_color;
+    public Color firing_color;
+
+    int sprite_ind = 0;
+    public List<Sprite> normal_sprites;
+    public List<Sprite> charging_sprites;
 
     Player player;
     Enemy enemy;
+    SpriteRenderer sprite_renderer;
 
     void Awake()
     {
         player = FindObjectOfType<Player>();
         enemy = GetComponent<Enemy>();
+        sprite_renderer = GetComponent<SpriteRenderer>();
 
         child = transform.GetChild(0);
         laser_renderer = child.GetComponent<SpriteRenderer>();
@@ -45,15 +54,18 @@ public class Sniper : MonoBehaviour
             }
 
             laser_renderer.enabled = true;
+            laser_renderer.color = charging_color;
 
             child.right = player.transform.position - transform.position;
-            child.localPosition = (player.transform.position - transform.position) / 2f;
-            child.localScale = new Vector3(Vector3.Distance(transform.position, player.transform.position) * 16, 1, 1);
+            child.localPosition = (player.transform.position - transform.position + new Vector3(0, 0.6f, 0)) / 2f;
+            child.localScale = new Vector3(Vector3.Distance(transform.position + new Vector3(0, 0.6f, 0), player.transform.position) * 16, 1, 1);
 
             charge_time += Time.deltaTime;
 
             if (charge_time > 3)
             {
+                firing = true;
+
                 source.clip = fire_shot;
                 source.pitch = Random.Range(0.9f, 1.1f);
                 source.Play();
@@ -65,13 +77,73 @@ public class Sniper : MonoBehaviour
                 last_shot = Time.timeSinceLevelLoad;
             }
         }
-        else
+        else if (firing &&
+            TimeChange.current.dimension == GetComponentInParent<Enemy>().enemy_dimension &&
+            enemy.CanSeePlayer())
+        {
+            laser_renderer.color = firing_color;
+            child.right = player.transform.position - transform.position;
+            child.localPosition = (player.transform.position - transform.position + new Vector3(0, 0.6f, 0)) / 2f;
+            child.localScale = new Vector3(Vector3.Distance(transform.position + new Vector3(0, 0.6f, 0), player.transform.position) * 16, 3, 1);
+        }
+        else if (firing &&
+            !enemy.CanSeePlayer())
+        {
+            firing = false;
+        }
+        else 
         {
             laser_renderer.enabled = false;
 
+            firing = false;
             charge_time = 0;
 
             source.Stop();
+        }
+
+        SetSprite();
+    }
+
+    void SetSprite()
+    {
+        if (enemy.flashing)
+        {
+            sprite_renderer.sprite = enemy.flash_sprite;
+            return;
+        }
+
+        if (TimeChange.current.dimension != GetComponentInParent<Enemy>().enemy_dimension)
+        {
+            sprite_renderer.sprite = normal_sprites[0];
+            return;
+        }
+
+        Vector3 check = transform.position - player.transform.position;
+        if (check.x > 0)
+        {
+            sprite_renderer.flipX = true;
+        }
+        else
+        {
+            sprite_renderer.flipX = false;
+        }
+
+        if (check.y > 0)
+        {
+            sprite_ind = 0;
+        }
+        else
+        {
+            sprite_ind = 1;
+        }
+
+        if (charge_time > 0)
+        {
+            sprite_renderer.sprite = normal_sprites[sprite_ind];
+        }
+        else
+        {
+            sprite_renderer.sprite = charging_sprites[sprite_ind];
         }
     }
 }
