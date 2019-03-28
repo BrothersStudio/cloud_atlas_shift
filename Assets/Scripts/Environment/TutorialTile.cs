5e2a92ddbfbcfdf54b1bac86e5360b9b3979b51f
Patrick <pwcarney@bu.edu>
2019-03-27 23:27:35 -0400

    Added tutorial tiles

---
+++ Assets/Scripts/Environment/TutorialTile.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TutorialTile : MonoBehaviour
+{
+    public List<Sprite> sprites;
+    SpriteRenderer sprite_renderer;
+
+    void Start()
+    {
+        sprite_renderer = GetComponent<SpriteRenderer>();
+        StartCoroutine(CycleSprites());
+    }
+
+    IEnumerator CycleSprites()
+    {
+        while (true)
+        {
+            sprite_renderer.sprite = sprites[0];
+            yield return new WaitForSeconds(0.5f);
+            sprite_renderer.sprite = sprites[1];
+            yield return new WaitForSeconds(0.5f);
+        }
+    }
+}
