5c447afef419572b6ade50c435bca07ecd522910
Patrick <pwcarney@bu.edu>
2019-04-02 22:47:31 -0400

    Tweaked how clouds float in main menu

--- Assets/Scripts/Main Menu/CloudFloat.cs
+++ Assets/Scripts/Main Menu/CloudFloat.cs
@@ -7,27 +7,39 @@ public class CloudFloat : MonoBehaviour
     Vector3 orig_position;
     Vector3 float_position;
 
-    float move_speed = 0.01f;
-    float move_dist = 0.1f;
+    float move_speed = 0.5f;
+    float move_dist = 0.05f;
+
+    bool start_float = false;
 
     void Start()
     {
         orig_position = transform.position;
-        float_position = orig_position + new Vector3(Random.Range(0, 10), Random.Range(0, 10), 0).normalized * move_dist;
+        float_position = orig_position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized * move_dist;
+
+        Invoke("StartFloat", Random.Range(0, 1f));
     }
 
-    void FixedUpdate()
+    void StartFloat()
     {
-        transform.position = Vector3.Lerp(transform.position, float_position, move_speed);
-        if (Vector3.Distance(transform.position, float_position) < 0.01f)
+        start_float = true;
+    }
+
+    void Update()
+    {
+        if (start_float)
         {
-            if (float_position == orig_position)
+            transform.position = Vector3.Lerp(transform.position, float_position, Time.deltaTime * move_speed);
+            if (Vector3.Distance(transform.position, float_position) < 0.01f)
             {
-                float_position = orig_position + new Vector3(Random.Range(0, 10), Random.Range(0, 10), 0).normalized * move_dist;
-            }
-            else
-            {
-                float_position = orig_position;
+                if (float_position == orig_position)
+                {
+                    float_position = orig_position + new Vector3(Random.Range(-10, 10), Random.Range(-10, 10), 0).normalized * move_dist;
+                }
+                else
+                {
+                    float_position = orig_position;
+                }
             }
         }
     }
